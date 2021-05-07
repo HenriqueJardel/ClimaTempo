c63c797a7e2f12ede73996652a23e7fd4a978102
rickjtbit <henrique.jardelt@gmail.com>
2021-05-07 17:43:09 -0400

    Primeiro Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './views/header/header.component';
+import { CardComponent } from './views/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
